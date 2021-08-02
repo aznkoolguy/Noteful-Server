7d92b15de8006d1129ac439cb6f2913bfaadbd07
aznkoolguy <34627946+aznkoolguy@users.noreply.github.com>
2021-08-02 02:30:17 -0500

    Create folders.fixtures.js

---
+++ test/folders.fixtures.js
@@ -0,0 +1,17 @@
+  
+function makeFoldersArray() {
+    return [
+      {
+        id: 1,
+        name: 'Test Folder 1',
+        modified: '2020-08-25T16:28:40.253Z'
+      },
+      {
+        id: 2,
+        name: 'Test Folder 2',
+        modified: '2020-08-24T16:28:40.253Z'
+      }
+    ]
+  }
+  
+  module.exports = { makeFoldersArray }
